eaef22cf9604a2c481dca662b414a02df46908e3
Funya1337 <funya7654@gmail.com>
2020-11-25 23:34:28 +0300

    first commit

---
+++ desktop/src/com/example/spacecode/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.example.spacecode.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.example.spacecode.SpaceCode;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceCode(), config);
+	}
+}
